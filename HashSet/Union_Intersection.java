f33b003752b32b2dd3b111ba8ad519a32a29de30
Dhananjay Kale <dhananjaykale9359@gmail.com>
2025-08-19 19:13:51 +0530

    Implementation of Union and Intersection from two array using HashSet

---
+++ HashSet/Union_Intersection.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Union_Intersection {
+
+  public static void main(String args[]) {
+    int arr1[] = { 7, 3, 9 };
+    int arr2[] = { 6, 3, 9, 2, 9, 4 };
+
+    HashSet<Integer> set = new HashSet<>();
+
+    for (int i = 0; i < arr1.length; i++) {
+      set.add(arr1[i]);
+    }
+    for (int i = 0; i < arr2.length; i++) {
+      set.add(arr2[i]);
+    }
+    System.out.println("The Union number of Elements are=" + set.size());
+
+    set.clear();
+    int count = 0;
+
+    for (int i = 0; i < arr1.length; i++) {
+      set.add(arr1[i]);
+    }
+    for (int i = 0; i < arr2.length; i++) {
+      if (set.contains(arr2[i])) {
+        count++;
+        set.remove(arr2[i]);
+      }
+    }
+    System.out.println("Size of the Intersection Set are = " + count);
+  }
+}
