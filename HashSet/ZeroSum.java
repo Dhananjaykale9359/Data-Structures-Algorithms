87f16d7b3c49855420063103ea0d2da2f3d44b18
Dhananjay Kale <dhananjaykale9359@gmail.com>
2025-08-20 22:12:59 +0530

    Implementation of ZeroSum using HashMap

---
+++ HashSet/ZeroSum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ZeroSum{
+    public static void main(String args[]){
+        int arr[] = {15,-2,2,-8,1,7,10,23};
+
+        HashMap<Integer, Integer>map = new HashMap<>();
+
+
+        int sum = 0;
+        int len = 0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            
+            if(map.containsKey(sum)){
+                len = Math.max(len, i-map.get(sum));
+            }
+            else{
+                map.put(sum , i);
+            }
+        }
+        System.out.println("The maximum length of Subarray having sum zero is ="+ len);
+    }
+}
